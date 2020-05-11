7e71dee9381aa9cdf006c3d5d83a05b4bb866e7e
StreetDream <mishkasaltovskiy@gmail.com>
2020-05-11 20:09:14 +0300

    added section experience

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,12 @@ $(function(){
     });
 
     $('.time__slider-inner').slick();
-    
+
+    $('.experience__slider-inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 
 });
 
